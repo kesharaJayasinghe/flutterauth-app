bd97137773b5f4db3cb98d3f04782ea03af6b807
kesharaJayasinghe <kesharalahiru1@gmail.com>
2020-09-07 18:33:42 +0530

    Theming

--- lib/screens/loginscreen.dart
+++ lib/screens/loginscreen.dart
@@ -14,7 +14,7 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(12.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
