12a31e483af3f58ffa7c6c3f8485108b2fce7b0e
kesharaJayasinghe <kesharalahiru1@gmail.com>
2020-09-09 19:43:58 +0530

    Styling

--- lib/screens/sign_in/components/body.dart
+++ lib/screens/sign_in/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutterauthapp/components/social_card.dart';
+import 'package:flutterauthapp/constants.dart';
 import 'package:flutterauthapp/screens/sign_in/components/signin_form.dart';
 import 'package:flutterauthapp/size_config.dart';
 
@@ -14,42 +15,67 @@ class Body extends StatelessWidget {
           padding: EdgeInsets.symmetric(
             horizontal: getProportionateScreenWidth(20),
           ),
-          child: Column(
-            children: [
-              Text(
-                "Welcome!",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: getProportionateScreenWidth(28),
-                  fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.04),
+                Text(
+                  "Welcome!",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: getProportionateScreenWidth(28),
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Text(
-                "Sign In with your username and password \nor continue with social media.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: getProportionateScreenWidth(15),
+                Text(
+                  "Sign In with your username and password \nor continue with social media.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: getProportionateScreenWidth(15),
+                  ),
                 ),
-              ),
-              SignForm(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SocialCard(
-                    icon: "assets/icons/google.svg",
-                    press: () {},
-                  ),
-                  SocialCard(
-                    icon: "assets/icons/facebook.svg",
-                    press: () {},
-                  ),
-                  SocialCard(
-                    icon: "assets/icons/twitter.svg",
-                    press: () {},
-                  ),
-                ],
-              ),
-            ],
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                SignForm(),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SocialCard(
+                      icon: "assets/icons/google.svg",
+                      press: () {},
+                    ),
+                    SocialCard(
+                      icon: "assets/icons/facebook.svg",
+                      press: () {},
+                    ),
+                    SocialCard(
+                      icon: "assets/icons/twitter.svg",
+                      press: () {},
+                    ),
+                  ],
+                ),
+                SizedBox(height: getProportionateScreenHeight(20)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account? ",
+                      style: TextStyle(
+                        fontSize: getProportionateScreenWidth(15),
+                      ),
+                    ),
+                    Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        fontSize: getProportionateScreenWidth(15),
+                        color: kPrimaryColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
